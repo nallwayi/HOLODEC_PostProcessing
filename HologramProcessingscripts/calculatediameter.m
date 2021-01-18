c70e29b5264b3c45f279e860a0c804efed73e516
Nithin Allwayin <54512005+nallwayi@users.noreply.github.com>
2021-01-18 17:53:34 -0500

    Delete HologramProcessingscripts directory

--- HologramProcessingscripts/calculatediameter.m
+++
@@ -1,9 +0,0 @@
-function pd=calculatediameter(particledata)
-
-    pd.eqdiameter  = particledata.eqsiz;
-    pd.majdiameter = particledata.majsiz;
-    pd.mndiameter = 0.5*(particledata.majsiz+particledata.minsiz);
-    pd.holonum     = particledata.holonum;
-    pd.holotimes   = particledata.holotimes;
-
-end
